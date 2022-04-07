7b515595015f92e35e35445ca0be572e2b1191d2
Orkhanvg <orxanvg@code.edu.az>
2022-04-07 17:28:58 +0400

    Add project files.

---
+++ ThreadAsyncDirectoryFile/Models/Status.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadAsyncDirectoryFile.Models
+{
+    class Status
+    {
+//        Id  
+//- Title
+//- Content
+//- SharedDate
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime SharedDate { get; set; }
+        public Status (DateTime shareddate)
+        {
+            SharedDate = shareddate;
+
+        }
+
+
+
+
+        public Status( string title, string content)
+        {
+          
+            Title = title;
+            Content = content;
+            
+
+        }
+        public void GetStatusInfo()
+        {
+
+        }
+    }
+}
---
+++ ThreadAsyncDirectoryFile/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ThreadAsyncDirectoryFile.Models
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+
+        List<Status> Statuses = new List<Status>();
+
+        public void StatusesAdd()
+        {
+
+
+        }
+
+
+        public void ShareStatus(Status status)
+
+        {
+
+            Statuses.Add(status);
+        }
+
+        public void GetStatusById()
+        {
+
+        }
+
+        public Status GetStatusById(int? id)
+        {
+            foreach (var item in Statuses)
+            {
+                if (id == item.ID)
+                {
+                   return item;
+                }
+                else
+                {
+                    throw new Exception("not found");
+                }
+            }
+        }
+
+        public GetAllStatuses()
+        {
+
+        }
+    }
+}
